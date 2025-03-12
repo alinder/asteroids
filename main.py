1e59fb351a90da7ae4136e16d8b53c29fd5a712c
alinder <11338228+alinder@users.noreply.github.com>
2025-03-12 10:15:56 -0500

    Working app with asteroid splits

--- main.py
+++ main.py
@@ -1,21 +1,49 @@
+import sys
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import *
+from shot import Shot
 
 def main():
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	dt = 0
 	clock = pygame.time.Clock()
+
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	asteroids = pygame.sprite.Group()
+	shots = pygame.sprite.Group()
+	Player.containers = (updatable, drawable)
+	Shot.containers = (shots, updatable, drawable)
+	Asteroid.containers = (asteroids, updatable, drawable)
+	AsteroidField.containers = (updatable)
+	asteroid_field = AsteroidField()
+
 	player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+
+
 
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 		
+		updatable.update(dt)
+		for asteroid in asteroids:
+			if asteroid.collision(player):
+				sys.exit("Game Over!")
+		
+			for shot in shots:
+				if asteroid.collision(shot):
+					asteroid.split()
+					shot.kill()
+		
 		screen.fill(("black"))
-		player.draw(screen)
+		for each in drawable:
+			each.draw(screen)
 		pygame.display.flip()
 		dt = clock.tick(60) / 1000
 		
